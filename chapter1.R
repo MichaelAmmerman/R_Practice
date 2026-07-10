9ee43fb5b358e9b9d9dfaffdeeb3f5e8fda3df6f
Michael Ammerman <michael.ammerman@lee.kyschools.us>
2026-07-10 15:56:13 -0400

    update

---
+++ chapter1.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
+penguins
+glimpse(penguins)
+ggplot(data = penguins,
+       mapping = aes(x = flipper_length_mm, y = body_mass_g)
+       )
+       
---
+++ package_Installs.R
@@ -0,0 +1,6 @@
+install.packages("tidyverse")
+
+
+install.packages(c("arrow", "babynames", "curl", "duckdb", "gapminder", "ggrepl", "ggridges", "ggthemes", "hexbin", "janitor", "lahman", "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins", "repurrsive", "tidymodels", "writexl"))
+                      
+library(tidyverse)
