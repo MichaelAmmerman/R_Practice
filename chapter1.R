591a62c56b2e8d12767c31c5fc8577dea86b68d8
Michael Ammerman <michael.ammerman@uky.edu>
2026-07-12 22:25:20 -0400

    update

--- chapter1.R
+++ chapter1.R
@@ -4,6 +4,7 @@ library(ggthemes)
 penguins
 glimpse(penguins)
 ggplot(data = penguins,
-       mapping = aes(x = flipper_length_mm, y = body_mass_g)
-       )
+       mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
+       ) +
+  geom_point()
        
