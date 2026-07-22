4ff4635c970c46c7f380fcdd6220059bdd2a6cb0
Michael Ammerman <michael.ammerman@uky.edu>
2026-07-22 19:19:37 -0400

    add project and chapter 4 and 5

--- chapter5/Chapter5.R
+++ chapter5/Chapter5.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggplot2)
 
 billboard
 
@@ -9,7 +10,7 @@ billboard |>
   values_to = "rank"
 )
 
-billboard |> 
+billboard_longer <- billboard |> 
   pivot_longer(
     cols = starts_with("wk"),
     names_to = "week",
@@ -18,4 +19,51 @@ billboard |>
   ) |> 
   mutate(
     week = parse_number(week)
+  )
+
+billboard_longer |> 
+  ggplot(aes(x = week, y = rank, group = track)) +
+  geom_line(alpha = .25) +
+  scale_y_reverse()
+
+df <- tribble(
+  ~id, ~bp1, ~bp2,
+  "A", 100, 120,
+  "B", 140, 115,
+  "C", 120, 125
+)
+
+df |> 
+  pivot_longer(
+    cols = bp1:bp2,
+    names_to = "measurement",
+    values_to = "value"
+    )
+
+who2 |> 
+  pivot_longer(
+    cols = !(country:year),
+    names_to = c("diagnosis", "gender", "age"),
+    names_sep = "_",
+    values_to = "count"
+  )
+
+household |> 
+  pivot_longer(
+    cols = !family,
+    names_to = c(".value", "child"),
+    names_sep = "_",
+    values_drop_na = TRUE
+  )
+cms_patient_experience
+
+
+cms_patient_experience |> 
+  distinct(measure_cd, measure_title)
+
+cms_patient_experience |> 
+  pivot_wider(
+    id_cols = starts_with("org"),
+    names_from = measure_cd,
+    values_from = prf_rate
   )
---
+++ chapter6/Chapter6.R
@@ -0,0 +1,8 @@
+library(dplyr)
+library(nycflights13)
+
+not_cancelled <- flights |> 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+not_cancelled
+
+
