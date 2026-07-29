6a20c92e6de64760119273d72f6700275d44bb05
Michael Ammerman <michael.ammerman@lee.kyschools.us>
2026-07-29 17:15:12 -0400

    update

---
+++ r4ds/Chapter11.R
@@ -0,0 +1,55 @@
+8 - 8 * 8 + 8
+install.packages(scales)
+install.packages("ggrepel")
+library(scales, ggrepel, patchwork)
+library(ggrepel)
+
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  labs(
+    x = "engine displacement (L)",
+    y = "Highway fuel economy (mpg)",
+    color = "Car type",
+    title = "Fuel efficiency generally decreased with engine size",
+    subtittle = "Two seater (sports cars) are an exception because of their light weight",
+    caption = "Data from fueleconomy.gov"
+  )
+
+label_info <- mpg |> 
+  group_by(drv) |> 
+  slice_head(n = 1) |> 
+  mutate(
+    drive_type = case_when(
+      drv == "f" ~ "front-wheel drive",
+      drv == "r" ~ "rear-wheel drive",
+      drv == "4" ~ "4-wheel drive"
+    )
+  ) |> 
+  select(displ, hwy, drv, drive_type)
+
+label_info
+
+
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(se = FALSE) +
+  geom_label_repel(
+    data = label_info,
+    aes(x = displ, y = hwy, label = drive_type),
+    fontface = "bold", size = 5, nudge_y = 2
+  ) +
+  theme(legend.position = "none")
+
+potential_outliers <- mpg |> 
+  filter(hwy > 40 | (hwy > 20 & displ > 5))
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_text_repel(data = potential_outliers, aes(label = model)) +
+  geom_point(data = potential_outliers, color = "red") +
+  geom_point(
+    data = potential_outliers,
+    color = "red", size = 3, shape = "circle open"
+  )
